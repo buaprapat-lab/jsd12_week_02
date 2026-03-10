36d8200a58a3bc8b5ff8fb70c8602c1aba8cc1d2
buaprapat-lab <buaprapat@gmail.com>
2026-03-10 19:51:24 +0700

    first commit week02

---
+++ 05_product-backlog.md
@@ -0,0 +1,32 @@
+Product Backlog: 
+01 / Must / 
+As a customer, I want to view a list of products to see what is available.
+Entity: Products
+
+02 / Must / 
+As a customer, I want to view product details to ensure quality and benefits.
+Entity: Products
+
+03 / Must /
+As a customer, I want to add items to a cart to prepare for checkout.
+Entity: Products, Order Item
+
+04 / Must /
+As a customer, I want to see the total amount to confirm my order.
+Entity: Order
+
+05 / Must / 
+As an admin, I want to view new orders to prepare for shipment.
+Entity: Order, Order Item
+
+06 / Should / 
+As a customer, I want to track my order status to see when it arrives.
+Entity: Tracking
+
+07 / Should /
+As a customer, I want to register an account to save my order history.
+Entity: User
+
+08 / Could /
+As a customer, I want to add my pet’s info (e.g., Bobo) for personalized advice.
+Entity: Pet
---
+++ 06_bring-it-to-javascript.js
@@ -0,0 +1,68 @@
+// Mockup Data
+const users = [
+    {
+        user_id: "U001",
+        user_name: "Sato Foster",
+        user_email: "sato.foster@doggieclub.com",
+        user_address: "101 Bellavue, Seattle",
+        role: "customer"
+    }
+];
+
+const pet = [
+    {
+        pet_id: "P001",
+        user_id: "U001",
+        pet_name: "Bobo",
+        pet_breed: "Dalmatian",
+        pet_age: 7,
+        pet_health_goal: "25 years life expectancy"
+    }
+];
+
+const products = [
+    {
+        product_id: "PRO01", product_name: "Spirulina Powder", product_price: 60,
+        stock_quatity: 17, inStock: true, product_category: "Superfood Boosters"
+    },
+    {
+        product_id: "PRO02", product_name: "Mixed Berry Yogurt", product_price: 25,
+        stock_quatity: 25, inStock: true, product_category: "Superfood Boosters"
+    },
+    {
+        product_id: "PRO03", product_name: "Freeze-dried Salmon", product_price: 90,
+        stock_quatity: 18, inStock: true, product_category: "High-performance Proteins"
+    },
+    {
+        product_id: "PRO04", product_name: "Jerky Sticks", product_price: 40,
+        stock_quatity: 0, inStock: false, product_category: "High-performance Proteins"
+    },
+    {
+        product_id: "PRO05", product_name: "Bone Broth", product_price: 55,
+        stock_quatity: 30, inStock: true, product_category: "Vitality Hydration"
+    },
+];
+
+//Shopping cart
+let shoppingCart = [
+    { product_id: "PRO01", product_quatity: 2 },
+    { product_id: "PRO05", product_quatity: 1 }
+];
+
+//Transaction data (simulation)
+let order = [
+    {
+        order_id: "BB-1001",
+        user_id: "U001",
+        items: shoppingCart, //to connect with the shoppingCart
+        total_amount: (60 * 2) + 55,
+        status: "pending",
+    }
+];
+
+// checking
+
+console.log(products); // check all items
+console.log(products[1].product_name); // check 2nd product, show product name
+console.log(shoppingCart); // check all item in the cart
+console.log(order[0]);
